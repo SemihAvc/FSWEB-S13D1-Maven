fceb3b49720d2cf74bb0c8ef1bac8a2248831494
SemihAvc <semavci95@gmail.com>
2024-03-19 22:42:36 +0300

    SPR13D1 solutions.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -50,11 +50,11 @@ public class Main {
         return (firstAge>=13 && firstAge<=19) ||  (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 29);
     }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
+    public static boolean isCatPlaying(boolean isSummer, int temperature) {
         if (isSummer) {
-            return temp > 25 && temp <= 45;
+            return temperature > 25 && temperature <= 45;
         }
-        return temp > 25 && temp <= 35;
+        return temperature > 25 && temperature <= 35;
     }
 
 
